63eba9c22efc11e128beb0196ac7fe60afd27d61
Rifat <reaget.acc@gmail.com>
2023-05-09 13:13:26 +0300

    created file component. added new view

---
+++ src/components/ui/FileSelectComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- eslint-disable -->
+  <div class="flex">
+    <input
+      type="file"
+      :id="name"
+      :disabled="disabled"
+      class="absolute h-0 w-0 invisible overflow-hidden opacity-0 -z-[1]"
+    >
+    <label
+      class="flex items-center w-full px-4 py-6 text-2xl text-gray-300 bg-primary-light rounded-lg outline-none"
+      :for="name"
+    >
+      {{ title }}
+      <svg
+        class="absolute right-10"
+        width="30"
+        height="30"
+      >
+        <use xlink:href="/img/icons/sprite.svg#camera"></use>
+      </svg>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-file',
+  data: () => ({
+    value: null,
+  }),
+  props: {
+    name: {
+      required: true,
+      type: String,
+    },
+    title: {
+      default: '',
+    },
+    disabled: {
+      default: false,
+    },
+  },
+};
+</script>
--- src/views/post/PostCreateView.vue
+++ src/views/post/PostCreateView.vue
@@ -6,12 +6,12 @@
         <!-- eslint-disable-next-line max-len -->
         <select id="job" class="h-[52px] indent-4 px-4 py-6 text-2xl text-black bg-primary-light rounded-lg outline-none" name="job">
           <option value="default" selected>Выберите категорию</option>
-          <option value="category1">Литература</option>
-          <option value="category2">Математика</option>
-          <option value="category3">Искусство</option>
+          <option value="category1">Статьи</option>
+          <option value="category2">Психологическая поддержка</option>
         </select>
       </label>
       <v-textarea name="description" :placeholder="'Описание'" />
+      <v-file title="Выберите обложку" name="file"></v-file>
       <button
         class="px-4 py-6 mt-14 text-2xl text-white bg-primary-default rounded-lg outline-none"
       >Создать</button>
@@ -22,11 +22,13 @@
 <script>
 import VInput from '@/components/ui/InputComponent.vue';
 import VTextarea from '@/components/ui/TextareaComponent.vue';
+import VFile from '@/components/ui/FileSelectComponent.vue';
 
 export default {
   components: {
     VInput,
     VTextarea,
+    VFile,
   },
 };
 </script>
